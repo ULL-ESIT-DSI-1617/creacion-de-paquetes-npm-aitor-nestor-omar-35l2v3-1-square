331f668b877ccdff7373594e77db5118c7080747
Omar Mendo <omen.mesa20@gmail.com>
2017-04-23 13:01:21 +0100

    Modificando directorios

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-'use strict'
-
-var figura = require('ull-shape-aitor-nestor-omar')
-var shape = figura.Shape
-
-class Square extends Figura {
-  constructor(options) {
-    Shapes.Square = Square;
-    super("Square",options)
-    this.width = options.width;
-  }
-  
-  getArea() {
-    return Math.pow(this.width,2);
-  }
-}
+module.exports = require('./lib/square.js');
